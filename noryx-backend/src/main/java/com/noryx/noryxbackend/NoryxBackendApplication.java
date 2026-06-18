8eeb368a88e800cc61da45fdf7593616384d6410
vanala23 <vanala23@htl-kaindorf.at>
2026-06-18 08:36:40 +0200

    feat: initial project structure

---
+++ noryx-backend/src/main/java/com/noryx/noryxbackend/NoryxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.noryx.noryxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoryxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoryxBackendApplication.class, args);
+    }
+
+}
---
+++ noryx-backend/src/test/java/com/noryx/noryxbackend/NoryxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noryx.noryxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoryxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
